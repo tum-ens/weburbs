44ccf434290822aa671a6507d881d3566bc437a6
Jonas Stahl <Jonas.M.Stahl@gmail.com>
2024-10-25 12:41:08 +0200

    🎨 Format

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,7 @@ import tailwindcss_primeui from 'tailwindcss-primeui'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,26 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import Components from 'unplugin-vue-components/vite';
+import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 8080
+    port: 8080,
   },
   plugins: [
     vue(),
     vueDevTools(),
     Components({
-      resolvers: [
-        PrimeVueResolver()
-      ]
-    })
+      resolvers: [PrimeVueResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
